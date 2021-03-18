cc531f0d5a4ae1a62dcae8fb14333003dd7a3418
Nick Marriotti <nmarriotti@gmail.com>
2021-03-18 10:56:49 -0400

    Do not need to specify scriptroot

--- config.ps1
+++ config.ps1
@@ -3,7 +3,7 @@ param (
     [string]$Commands
 )
 
-python $PSScriptRoot/configure_device.py -c $PSScriptRoot/configs/$Commands
+python $PSScriptRoot/configure_device.py -c $Commands
 
 # Return the exit code of the python script [0=success, 1=error]
 exit $LASTEXITCODE
